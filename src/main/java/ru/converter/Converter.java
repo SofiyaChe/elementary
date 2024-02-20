82273b79fdb704c6a0ed13c5984c08e0a6c8c294
Admin <sofiya.chebotareva@bk.ru>
2024-02-20 09:29:14 +0100

    1.9 Конвертер валюты 2

--- src/main/java/ru/converter/Converter.java
+++ src/main/java/ru/converter/Converter.java
@@ -2,13 +2,11 @@ package ru.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 100;
-        return rsl;
+        return value / 100;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 90;
-        return rsl;
+        return value / 90;
     }
 
     public static void main(String[] args) {
