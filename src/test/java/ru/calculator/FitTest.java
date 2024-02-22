263779c4027509a99e2a4c5f4b00729240d25997
Admin <sofiya.chebotareva@bk.ru>
2024-02-22 14:45:36 +0100

    2.3 Входные данные в тесте 2

--- src/test/java/ru/calculator/FitTest.java
+++ src/test/java/ru/calculator/FitTest.java
@@ -1,9 +1,7 @@
 package ru.calculator;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class FitTest {
 
