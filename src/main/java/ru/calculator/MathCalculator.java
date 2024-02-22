276eb113f4afa3732ead03ef6f86634fb2aeed5c
Admin <sofiya.chebotareva@bk.ru>
2024-02-22 14:44:28 +0100

    2.2 Что такое import 2

--- src/main/java/ru/calculator/MathCalculator.java
+++ src/main/java/ru/calculator/MathCalculator.java
@@ -1,21 +1,20 @@
 package ru.calculator;
+
 import static ru.math.MathFunction.*;
 
 public class MathCalculator {
 
     public static double sumAndMultiply(double first, double second) {
-        return ru.math.MathFunction.sum(first, second)
-                + ru.math.MathFunction.multiply(first, second);
+        return sum(first, second) + multiply(first, second);
     }
+
     public static double sumOfTwoMethods(double first, double second) {
-        return ru.math.MathFunction.subtract(first, second)
-                + ru.math.MathFunction.divide(first, second);
+        return subtract(first, second) + divide(first, second);
     }
+
     public static double sumOfFourMethods(double first, double second) {
-        return ru.math.MathFunction.subtract(first, second)
-                + ru.math.MathFunction.divide(first, second)
-                + ru.math.MathFunction.sum(first, second)
-                + ru.math.MathFunction.multiply(first, second);
+        return subtract(first, second) + divide(first, second) + sum(first, second)
+                + multiply(first, second);
     }
 
     public static void main(String[] args) {
