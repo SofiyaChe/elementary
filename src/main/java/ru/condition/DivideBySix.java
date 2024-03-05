eaf585c0baf60517e3ac22111450314e9cb5eab7
Admin <sofiya.chebotareva@bk.ru>
2024-03-05 18:44:39 +0100

    3_15_Вложенный_оператор_if_else_Запутанный_код

--- src/main/java/ru/condition/DivideBySix.java
+++ src/main/java/ru/condition/DivideBySix.java
@@ -3,11 +3,13 @@ package ru.condition;
 public class DivideBySix {
     public static String checkNumber(int number) {
         String rsl;
-        if (number % 2 == 0 && number % 3 == 0) {
+        boolean divideBy3 = number % 3 == 0;
+        boolean divideBy2 = number % 2 == 0;
+        if (divideBy2 && divideBy3) {
             rsl = "The number divides by 6.";
-        } else if (number % 2 != 0 && number % 3 == 0) {
+        } else if (!divideBy2 && divideBy3) {
             rsl = "The number divides by 3, but it isn't the even number.";
-        } else if (number % 2 == 0 && number % 3 != 0) {
+        } else if (divideBy2 && !divideBy3) {
             rsl = "The number doesn't divide by 3, but it is the even number.";
         } else {
             rsl = "The number doesn't divide by 3 and it isn't the even number.";
