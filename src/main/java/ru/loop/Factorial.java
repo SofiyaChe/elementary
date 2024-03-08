c0e1cbb80eb225e4f85ef75fa33dc919f3181fb8
Admin <sofiya.chebotareva@bk.ru>
2024-03-08 14:21:16 +0100

    4_5_Создать_программу,_вычисляющую_факториал

---
+++ src/main/java/ru/loop/Factorial.java
@@ -0,0 +1,12 @@
+package ru.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+}
