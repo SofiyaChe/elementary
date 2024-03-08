14e98a6baad13d8f51becf6e6d18056609478c73
Admin <sofiya.chebotareva@bk.ru>
2024-03-08 15:23:17 +0100

    4_8_Простое_число_оператор_прерывания_break

---
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 1) {
+            prime = false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+
+}
