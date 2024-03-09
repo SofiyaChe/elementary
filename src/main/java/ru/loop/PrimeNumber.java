9c47409316b0675e944c7c4e06b6d065c4931c76
Admin <sofiya.chebotareva@bk.ru>
2024-03-09 18:58:04 +0100

    4.9 Простые числа

---
+++ src/main/java/ru/loop/PrimeNumber.java
@@ -0,0 +1,16 @@
+package ru.loop;
+
+public class PrimeNumber {
+
+    public static int calc(int finish) {
+
+        int count = 0;
+        for (int i = 1; i <= finish; i++) {
+            if (CheckPrimeNumber.check(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
