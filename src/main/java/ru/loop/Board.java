4444a19ae9d620d1ae4e227182a1dbcf31e4a303
Admin <sofiya.chebotareva@bk.ru>
2024-03-09 06:57:19 +0100

    4_6_Построить_шахматную_доску_в_псевдографике

--- src/main/java/ru/loop/Board.java
+++ src/main/java/ru/loop/Board.java
@@ -2,17 +2,14 @@ package ru.loop;
 
 public class Board {
     public static void paint(int width, int height) {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                /* условие проверки, что писать пробел или X */
-                /* Выше в задании мы определили закономерность, когда нужно проставлять X */
-                if (Math.abs(i - j) % 2 == 0) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i + j % 2 == 0) {
                     System.out.print("X");
                 } else {
                     System.out.print(" ");
                 }
             }
-            /* добавляем перевод на новую строку. */
             System.out.println();
         }
     }
