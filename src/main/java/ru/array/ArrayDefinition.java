24984df45cd0e1a70ca4424b36100eb675d26683
Admin <sofiya.chebotareva@bk.ru>
2024-03-10 15:14:31 +0100

    5.3 Заполнение массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива surname равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Artem Ivanov";
+        names[1] = "Artem Petrov";
+        names[2] = "Oleg Ivanov";
+        names[3] = "Oleg Petrov";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
