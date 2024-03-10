ae39bc015aa2be953626543ef468310ede3012f8
Admin <sofiya.chebotareva@bk.ru>
2024-03-10 14:18:56 +0100

    4.14 Генерация конструкций в IDEA

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -2,6 +2,10 @@ package ru;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world");
+        int age = 18;
+        float length = 73.002f;
+
     }
 }
