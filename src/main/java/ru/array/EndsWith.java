6ac43d00ded268f54b34842a9fb7266ce3433ef9
Admin <sofiya.chebotareva@bk.ru>
2024-03-14 19:12:33 +0100

    5.17 Слово заканчивается на ...2

--- src/main/java/ru/array/EndsWith.java
+++ src/main/java/ru/array/EndsWith.java
@@ -3,6 +3,12 @@ package ru.array;
 public class EndsWith {
 
     public static boolean endsWith(char[] word, char[] post) {
-        return (word[word.length - 1] == post[post.length - 1]);
+        int count = 0;
+        for (int i = 0; i < post.length; i++) {
+            if (word[word.length - 1 - i] == post[post.length - 1 - i]) {
+                count++;
+            }
+        }
+        return count == post.length;
     }
 }
