c6f48c66447fdce3d69325ec8b129a23b5bde2ef
Admin <sofiya.chebotareva@bk.ru>
2024-03-15 11:23:09 +0100

    5_19_Поиск_минимального_числа_в_массиве

---
+++ src/test/java/ru/array/Min.java
@@ -0,0 +1,13 @@
+package ru.array;
+
+public class Min {
+    public static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+}
