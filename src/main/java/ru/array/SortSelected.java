605569c1a071ae81cfe5c022305896cec8eb0768
Admin <sofiya.chebotareva@bk.ru>
2024-03-15 19:11:52 +0100

    5.21 Сортировка выборкой 4

--- src/main/java/ru/array/SortSelected.java
+++ src/main/java/ru/array/SortSelected.java
@@ -1,11 +1,13 @@
 package ru.array;
 
+import static ru.array.MinDiapason.findMin;
+
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = FindLoop.indexInRange(data, MinDiapason.findMin(data, i, data.length - 1),
-                    i, data.length - 1);
-            SwitchArray.swap(data, i, min);
+            int minIndex = FindLoop.indexInRange(data, findMin(data, i,
+                            data.length - 1), i, data.length - 1);
+            SwitchArray.swap(data, i, minIndex);
         }
         return data;
     }
