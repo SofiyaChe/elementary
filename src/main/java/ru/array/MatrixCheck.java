02f9e0e045c8f4d72055afbf8d7079e96163e67d
Admin <sofiya.chebotareva@bk.ru>
2024-04-01 17:31:01 +0200

    5_30_Выигрышные_комбинации_в_сокобан`_fin

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -29,10 +29,8 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 'X') {
-                if (monoVertical(board, i) || monoHorizontal(board, i)) {
-                    return true;
-                }
+            if (board[i][i] == 'X' && (monoVertical(board, i) || monoHorizontal(board, i))) {
+                return true;
             }
         }
         return false;
