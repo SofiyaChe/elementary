65e88321ee227e2f6523f6ca755a955b2894c4a2
Admin <sofiya.chebotareva@bk.ru>
2024-04-19 14:08:10 +0200

    7_15_Перегрузить_метод_max_для_трех_чисел

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -1,9 +1,16 @@
 package ru.condition;
 
 public class Max {
-    
+
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
 
+    public static int max(int num1, int num2, int num3, int num4) {
+        return max(max(num1, num2), max(num3, num4));
+    }
+
+    public static int max(int num1, int num2, int num3) {
+        return max(max(num1, num2), num3);
+    }
 }
--- src/test/java/ru/condition/MaxTest.java
+++ src/test/java/ru/condition/MaxTest.java
@@ -1,7 +1,9 @@
 package ru.condition;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 class MaxTest {
     @Test
@@ -28,6 +30,27 @@ class MaxTest {
         int right = 2;
         int result = Max.max(left, right);
         int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void check3nums() {
+        int num1 = 5;
+        int num2 = 3;
+        int num3 = 6;
+        int result = Max.max(num1, num2, num3);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void check4nums() {
+        int num1 = 5;
+        int num2 = 3;
+        int num3 = 6;
+        int num4 = 7;
+        int result = Max.max(num1, num2, num3, num4);
+        int expected = 7;
         assertThat(result).isEqualTo(expected);
     }
 }
